c93157092d0571f20f53aff196811458cb51dec2
fasulu <79796718+fasulu@users.noreply.github.com>
2021-09-07 11:41:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     port: 3501,
-    mongoURL: 'MONGO_URI=mongodb+srv://graphql-tutorial:1234567890@cluster0.uvdug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoURL: 'MONGO_URI=mongodb+srv://graphql-tutorial:<password>@cluster0.uvdug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
 
 module.exports = config
